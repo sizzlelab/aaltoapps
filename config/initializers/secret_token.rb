290b13f0770b0391c96a624313a8e7fe7b8faae7
Julius de Bruijn <pimeys@gmail.com>
2010-10-13 16:23:42 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AaltoApps::Application.config.secret_token = 'de8fa2a2182fa8e0ce248a75a336d122476682b4697f112cad02e3d2a9c4e37c07706540c110d077fab0f2ae3539b23b568f82bf858e48895cc2214abbabe9d5'

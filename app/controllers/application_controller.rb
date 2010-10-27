8c37d5ee28b2e03494f3c91424e219b73aa2ca17
Julius de Bruijn <pimeys@gmail.com>
2010-10-27 15:40:23 +0300

    Introduced current_user and logged_in? methods to controllers and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def login_required
+    if !current_user
+      return true
+    end
+    flash[:warning] = 'Login required'
+    redirect_to :controller => 'user', :action => 'login'
+  end
+  
+  def current_user
+    session[:user]
+  end
+  
+  def logged_in?
+    !session[:user].nil?
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_user
+    session[:user]
+  end
+  
+  def logged_in?
+    !current_user.nil?
+  end
 end

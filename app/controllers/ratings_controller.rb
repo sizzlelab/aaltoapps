b4d85c25d35c9534c878ecf88a5034240ebd35e6
Julius de Bruijn <pimeys@gmail.com>
2010-10-29 11:48:01 +0300

    Cleaned ratings controller, no need for ratings index or show

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,26 +1,4 @@
 class RatingsController < ApplicationController
-  # GET /ratings
-  # GET /ratings.xml
-  def index
-    @ratings = Rating.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @ratings }
-    end
-  end
-
-  # GET /ratings/1
-  # GET /ratings/1.xml
-  def show
-    @rating = Rating.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @rating }
-    end
-  end
-
   # GET /ratings/new
   # GET /ratings/new.xml
   def new

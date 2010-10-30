698d14fc3076665297c08824f1cc8acdfedf4d93
pekman <pekman@cc.hut.fi>
2010-10-31 00:04:25 +0300

    Changed rating system to use partials and implemented easy rating deletion

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -25,13 +25,22 @@ class RatingsController < ApplicationController
       # otherwise update the existing rating
       Rating.transaction do
         @rating = Rating.find_or_initialize_by_product_id_and_user_id(
-          :product_id => params[:rating][:product_id],
-          :user_id => current_user.id
-        )
-        @rating.rating = params[:rating][:rating]
-        success = @rating.save
-        raise ActiveRecord::Rollback unless success
+          :product_id => params[:rating][:product_id] || params[:product_id],
+          :user_id => current_user.id )
+        if params[:rating][:rating].empty?
+          # if the new rating value is empty, destroy the rating if it exists
+          @rating.destroy unless @rating.new_record?
+          success = true
+        else
+          @rating.rating = params[:rating][:rating]
+          success = @rating.save
+          raise ActiveRecord::Rollback unless success
+        end
       end
+    else
+      # create empty Rating for error message
+      @rating = Rating.new
+      @rating.errors.add :rating, 'can not be given when not logged in'
     end
 
     respond_to do |format|
@@ -39,7 +48,12 @@ class RatingsController < ApplicationController
         format.html { redirect_to(:back, :notice => 'Rating was successfully created/updated.') }
         format.xml  { render :xml => @rating, :status => :created, :location => @rating }
       else
-        format.html { redirect_to(:back, :notice => 'Rating was not created/updated.') }
+        format.html {
+          # put error messages to :alert flash message
+          errmsg = @rating.errors.full_messages.join(', ')
+          errmsg = " (#{errmsg})" unless errmsg.empty?
+          redirect_to(:back, :alert => 'Rating was not created/updated.' + errmsg)
+        }
         format.xml  { render :xml => @rating.errors, :status => :unprocessable_entity }
       end
     end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,4 +2,8 @@ class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
   validates_presence_of :rating, :user_id, :product_id
+  
+  def to_f
+    self.rating
+  end
 end

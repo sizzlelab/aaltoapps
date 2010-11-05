6c5acbfb39ef38b80816aa89cdd50f231c556f4f
drushil <drushil@gmail.com>
2010-11-05 15:18:26 +0200

    Added platform and donate field in Add a Product screen.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 	belongs_to :publisher, :class_name => "User", :foreign_key => "publisher_id"
-	validates :name, :description, :url, :presence => true
+	validates :name, :description, :url, :platform, :donate,:presence => true
   has_many :ratings, :dependent => :destroy
   has_many :comments, :dependent => :destroy
 end

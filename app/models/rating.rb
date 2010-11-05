54ddcfa6f86f72beefd81483191c4fded45847a3
Pekka Ekman <pekman@cc.hut.fi>
2010-11-05 16:06:26 +0200

    Added validation for Rating value and added a function that returns a list of proper values

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,11 +2,26 @@ class Rating < ActiveRecord::Base
   Max = 5
   Increment = 0.5
 
+  def self.allowed_values
+    (0 .. (Max/Increment)).map do |i|
+      i *= Increment
+      [i, i]
+    end
+  end
+
   belongs_to :user
   belongs_to :product
   validates :rating, :user_id, :product_id, :presence => true
   
   def to_f
     self.rating
+  end
+
+  class ValueValidator < ActiveModel::Validator
+    def validate(record)
+      unless allowed_values.member?(record.rating)
+        record.errors[:rating] << 'Invalid rating value'
+      end
+    end
   end
 end

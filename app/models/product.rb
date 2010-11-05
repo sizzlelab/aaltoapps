e6faae2a8e67033eac21d956a3cf6f7d418de2e2
Pekka Ekman <pekman@cc.hut.fi>
2010-11-05 12:47:51 +0200

    Changed validated_presence_of in models to "validates ..., :presence => true"

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 	belongs_to :publisher, :class_name => "User", :foreign_key => "publisher_id"
-	validates_presence_of :name, :description, :url
+	validates :name, :description, :url, :presence => true
   has_many :ratings, :dependent => :destroy
   has_many :comments, :dependent => :destroy
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,7 +1,7 @@
 class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
-  validates_presence_of :rating, :user_id, :product_id
+  validates :rating, :user_id, :product_id, :presence => true
   
   def to_f
     self.rating

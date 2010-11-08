10cbb8cfe551d576eed81ce92dbc05b53326c772
pekman <pekman@cc.hut.fi>
2010-11-08 19:08:42 +0200

    Fixed validation of Rating value

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,27 +1,32 @@
 class Rating < ActiveRecord::Base
+  Min = 0
   Max = 5
   Increment = 0.5
 
   def self.allowed_values
-    (0 .. (Max/Increment)).map do |i|
-      i *= Increment
-      [i, i]
+    values = []
+    (Min..Max).step(Increment) do |val|
+      values << val
+    end
+    values
+  end
+
+  class RatingValueValidator < ActiveModel::Validator
+    def validate(record)
+      if record.rating.nil?
+        record.errors[:rating] << "can't be blank"
+      elsif ! Rating::allowed_values.member?(record.rating)
+        record.errors[:rating] << "has invalid value"
+      end
     end
   end
 
   belongs_to :user
   belongs_to :product
-  validates :rating, :user_id, :product_id, :presence => true
-  
+  validates :user_id, :product_id, :presence => true
+  validates_with RatingValueValidator
+
   def to_f
     self.rating
-  end
-
-  class ValueValidator < ActiveModel::Validator
-    def validate(record)
-      unless allowed_values.member?(record.rating)
-        record.errors[:rating] << 'Invalid rating value'
-      end
-    end
   end
 end

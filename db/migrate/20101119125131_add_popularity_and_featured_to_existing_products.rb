cbcc3fd4e80c87f8c44c30d382d9d56157519e43
Pekka Ekman <pekman@cc.hut.fi>
2010-11-19 15:06:05 +0200

    Added a migration that fixes errors with old Product db rows

---
+++ db/migrate/20101119125131_add_popularity_and_featured_to_existing_products.rb
@@ -0,0 +1,13 @@
+class AddPopularityAndFeaturedToExistingProducts < ActiveRecord::Migration
+  def self.up
+    Product.update_all("popularity = 0", "popularity IS NULL")
+    Product.update_all("featured = FALSE", "featured IS NULL")
+    change_column :products, :popularity, :integer, :default => 0,     :null => false
+    change_column :products, :featured,   :boolean, :default => false, :null => false
+  end
+
+  def self.down
+    change_column :products, :popularity, :integer, :default => 0,     :null => true
+    change_column :products, :featured,   :boolean, :default => false, :null => true
+  end
+end

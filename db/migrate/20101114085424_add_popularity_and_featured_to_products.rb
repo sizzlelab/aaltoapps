3473761e56e02805e6d7f94de5775168d47960ac
Sami <sami.verkkopera@tkk.fi>
2010-11-19 12:57:00 +0200

    Typo in migration inteher to integer

--- db/migrate/20101114085424_add_popularity_and_featured_to_products.rb
+++ db/migrate/20101114085424_add_popularity_and_featured_to_products.rb
@@ -1,6 +1,6 @@
 class AddPopularityAndFeaturedToProducts < ActiveRecord::Migration
   def self.up
-    add_column :products,:popularity,:inteher,:default=>0
+    add_column :products,:popularity,:integer,:default=>0
     add_column :products,:featured,:boolean,:default=>false
   end
 

1115499e39ca3e691b169d7c1a90f47e5ba1a558
Pekka Ekman <pekman@cc.hut.fi>
2010-11-26 17:05:30 +0200

    Bugfix: old rating value was used when calculating average rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -8,7 +8,7 @@ class Rating < ActiveRecord::Base
   validates :rating, :user_id, :product_id, :presence => true
   validates :rating, :inclusion => {:in => (MIN..MAX).step(STEP).to_a }
   validates :rating, :inclusion => {:in => (@@min..@@max).step(@@step).to_a}
-  before_save :add_avg_rating_to_product
+  after_save :add_avg_rating_to_product
 
   def add_avg_rating_to_product
     self.product.avg_rating = self.product.ratings.average(:rating)  

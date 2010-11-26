b44f95bf7ff585d1eba81b87bade7b6452bfc3d2
Riku Salminen <maililistaalterego@gmail.com>
2010-11-26 14:47:33 +0200

    Added an Rspec test for Comments and some validators to actually pass the damn test :)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :product
   belongs_to :commenter, :class_name => "User", :foreign_key => "commenter_id"
 
+  validates :commenter_id, :product_id, :presence => true
+  validates :body, :length => { :minimum => 3 }
+
   def commenter_rating
     return product.ratings.find_by_user_id commenter
   end

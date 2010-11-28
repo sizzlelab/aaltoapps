b0237dae87bc604c16c8841891c4336a57631d73
pguo <peilinguo@gmail.com>
2010-11-28 11:12:31 +0200

    fixed "filter" and "sort"

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,12 +5,9 @@ class Product < ActiveRecord::Base
   validates :name, :description, :url, :platform, :donate,:presence => true
   has_many :ratings, :dependent => :destroy
   has_many :comments, :dependent => :destroy
+def average_rating
+    ratings.average(:rating)
+  end
 
-def self.find_apps_by(platform)
-   Platform.find(platform).products
-end
-def self.sort_apps_by(platform,criteria)
-   #order("#{criteria}").find_all_by_platform(platform)
-   Platform.find(platform).products.order("#{criteria}")
-end
+
 end

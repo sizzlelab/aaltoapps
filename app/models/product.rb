e101686caec8ac6cc12f1242e58394970bdd3ce3
pguo <peilinguo@gmail.com>
2010-11-28 12:48:12 +0200

    check "category" is chosen when publishing/editing a product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :publisher, :class_name => "User", :foreign_key => "publisher_id"
   belongs_to :category
   belongs_to :platform
-  validates :name, :description, :url, :platform, :donate,:presence => true
+  validates :name, :description, :url, :platform, :category, :donate,:presence => true
   has_many :ratings, :dependent => :destroy
   has_many :comments, :dependent => :destroy
 end

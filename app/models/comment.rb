605f99c2842f7b418c865f9c6c7215817c8a48b1
Riku Salminen <maililistaalterego@gmail.com>
2010-12-03 13:23:11 +0200

    Improved tests and validators for Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :product
   belongs_to :commenter, :class_name => "User", :foreign_key => "commenter_id"
 
-  validates :commenter_id, :product_id, :presence => true
+  validates :commenter, :product, :presence => true
   validates :body, :length => { :minimum => 3 }
 
   def commenter_rating

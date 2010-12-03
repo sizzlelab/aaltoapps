a348b0fb8235fe23729d640514294ab2773794f3
Julius de Bruijn <pimeys@gmail.com>
2010-12-03 13:35:16 +0200

    Product should not validate platform on save

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :publisher, :class_name => "User", :foreign_key => "publisher_id"
   belongs_to :category
   has_and_belongs_to_many :platforms
-  validates :name, :description, :url, :platform, :category, :donate,:presence => true
+  validates :name, :description, :url, :category, :donate,:presence => true
   has_many :ratings, :dependent => :destroy
   has_many :comments, :dependent => :destroy
 end

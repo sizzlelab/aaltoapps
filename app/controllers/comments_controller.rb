d09d3af5ec638f656595b7bbf27df1551a413684
Pekka Ekman <pekman@cc.hut.fi>
2011-01-21 15:59:35 +0200

    Fixed issue #11 (implemented comment editing)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,5 +5,32 @@ class CommentsController < ApplicationController
     @comment.save
     redirect_to product_path(@product)
   end
-end
 
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+
+    respond_to do |format|
+      if @comment.update_attributes(params[:comment])
+        format.html { redirect_to(@comment.product, :notice => 'Your comment was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(@comment.product, :notice => 'Comment deleted.') }
+      format.xml  { head :ok }
+    end
+  end
+end

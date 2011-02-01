ae40574fc2b9e8a26e0eae0d9cc644f8f3869c70
pguo <peilinguo@gmail.com>
2011-02-01 12:58:31 +0200

    issue 50

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,5 +1,6 @@
 class Platform < ActiveRecord::Base
   validates_presence_of :name
+  validates_uniqueness_of :name
   has_and_belongs_to_many :products, :uniq => true
   validates :name, :length => { :minimum => 3 }
 end

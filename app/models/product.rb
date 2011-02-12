b5762f3bbfd15270b138a68b9dd527c6a413a39a
Pekka Ekman <pekman@cc.hut.fi>
2011-02-12 20:46:18 +0200

    :platform_ids -> :platforms

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :publisher, :class_name => "User", :foreign_key => "publisher_id"
   belongs_to :category
   has_and_belongs_to_many :platforms, :uniq => true
-  validates :name, :description, :url, :platform_ids, :presence => true
+  validates :name, :description, :url, :platforms, :presence => true
   validates :name, :length => { :minimum => 3 }
   validates :url, :length => { :minimum => 12 } # http://ab.cd 
   has_many :ratings, :dependent => :destroy

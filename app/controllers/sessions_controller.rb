a213bc40de78d5e9c3f0a7fbe981ddfdd544b547
Pekka Ekman <pekman@cc.hut.fi>
2011-06-01 18:34:46 +0300

    Redirect to previous page on login.
    Closes #121.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,11 +26,29 @@ class SessionsController < ApplicationController
 
     flash[:notice] = _("Login successful")
 
-    # redirect using user's preferred locale, if possible
-    if session[:return_to]
-      session[:return_to].merge!(:locale => current_user.language)  if session.is_a? Hash
-      redirect_to session[:return_to]
-      session[:return_to] = nil
+    # Redirect to return_to using the user's preferred locale
+    #
+    # return_to is passed as an URL. That and the way HTTP 30x redirects work
+    # have the effect of changing the HTTP method to GET, which effectively
+    # converts potentially destructive actions to safe show or index actions
+    # as long as no GET action does anything destructive without a redirect.
+    return_to = params[:return_to].present? && begin
+      uri = URI::parse(params[:return_to])
+      if uri.host != request.host  # sanity check
+        nil
+      else
+        # try to parse the URL to return to
+        p = Rails.application.routes.recognize_path(params[:return_to])
+        p.reverse_merge!(Rack::Utils.parse_query(uri.query))
+        # don't redirect to login page
+        p = nil if p[:controller] == 'sessions' && p[:action] == 'index'
+        p
+      end
+    rescue
+      nil
+    end
+    if return_to
+      redirect_to return_to.merge(:locale => current_user.language)
     else
       redirect_to root_path(:locale => current_user.language)
     end

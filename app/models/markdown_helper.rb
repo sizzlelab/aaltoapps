2823d911f64c7d0d40bfbbf66e01ec998552ecd6
Pekka Ekman <pekman@cc.hut.fi>
2011-09-22 14:02:58 +0300

    Changed the whole product box into a link in product lists

--- app/models/markdown_helper.rb
+++ app/models/markdown_helper.rb
@@ -6,7 +6,7 @@ module MarkdownHelper
     # Creates a method that returns parsed HTML for each field.
     def markdown_fields(*fields)
       fields.each do |field|
-        define_method("#{field}_html") { markdown_field_as_html field }
+        define_method("#{field}_html") { |*args| markdown_field_as_html field, *args }
       end
     end
   end
@@ -17,12 +17,19 @@ module MarkdownHelper
 
   private
 
-  def markdown_field_as_html(field)
+  def markdown_field_as_html(field, *options)
     markdown = send field
     if markdown.blank?
       nil
     else
-      Redcarpet.new(markdown, *REDCARPET_OPTIONS).to_html.html_safe
+      allowed_options = [:no_links, :no_image]
+      # parse both hash and array options
+      hash = options.extract_options!
+      rc_opts = (Set.new(options) & allowed_options) | REDCARPET_OPTIONS
+      hash.slice(allowed_options).each do |opt, val|
+        rc_opts.send(val ? :add : :delete, opt)
+      end
+      Redcarpet.new(markdown, *rc_opts).to_html.html_safe
     end
   end
 

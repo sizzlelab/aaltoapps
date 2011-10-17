395e5da8b8d5309a84db3c20d8e87b8dd07c6ac6
Pekka Ekman <pekman@cc.hut.fi>
2011-10-17 17:40:38 +0300

    Disable cas_proxy_callback_controller if CAS not enabled

--- app/controllers/cas_proxy_callback_controller.rb
+++ app/controllers/cas_proxy_callback_controller.rb
@@ -2,18 +2,20 @@
 # The controller defines a single action: receive_pgt, which receives
 # proxy-granting ticket from CAS server.
 
-# rubycas-filter/rubycas-filter-rails compatibility hack
-module CASClient
-  module Frameworks
-    module Rails
-      class Filter < RubyCAS::Filter; end
+if AaltoApps::Application::CAS_ENABLED
+  # rubycas-filter/rubycas-filter-rails compatibility hack
+  module CASClient
+    module Frameworks
+      module Rails
+        class Filter < RubyCAS::Filter; end
+      end
     end
   end
-end
 
-require 'casclient/frameworks/rails/cas_proxy_callback_controller'
+  require 'casclient/frameworks/rails/cas_proxy_callback_controller'
 
-# Rails 3 compatibility hack
-class CasProxyCallbackController
-  RAILS_ROOT = ::Rails.root
+  # Rails 3 compatibility hack
+  class CasProxyCallbackController
+    RAILS_ROOT = ::Rails.root
+  end
 end

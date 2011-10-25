e587ef6539fbcfcdb5fcd9de3a44203e604656be
Pekka Ekman <pekman@cc.hut.fi>
2011-10-25 20:25:35 +0300

    Added menu open/close functionality to mobile site

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,50 +7,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require menu
 
 (function ($) {
-
-  function submenuOpen(j) {
-    j .parent('.menutop') .removeClass('menutop-closed') .addClass('menutop-open');
-    j .removeClass('menutop-closed') .addClass('menutop-open') .siblings('ul') .slideDown(250);
-  }
-
-  function submenuClose(j) {
-    j .removeClass('menutop-open') .addClass('menutop-closed')  .siblings('ul') .slideUp(250);
-    j .parent('.menutop') .removeClass('menutop-open') .addClass('menutop-closed');
-  }
-
   $(document).ready(function() {
-    // update open/closed status of menus from cookie
-    $.each(document.cookie.split(';'), function (i, cookie) {
-      var match = $.trim(cookie).match(/^openMenus=([a-zA-Z0-9_,-]+)$/);
-      if (match) {
-        $.each(match[1].split(','), function (i, id) {
-          $('#menu #' + id + ' .menutop') .addClass('menutop-open');
-        });
-      }
-    });
-
-    $('.menutop:not(.menutop-open)') .addClass('menutop-closed') .siblings('ul') .hide();
-
-    $('#menu .menutop') .click(function() {
-
-      if ($(this) .hasClass('menutop-open')) {
-        submenuClose($(this));
-      }else{
-        submenuClose($('#menu .menutop-open'));
-        submenuOpen($(this));
-      }
-
-      // set cookie to a list of ids of open menus
-      document.cookie =
-        'openMenus=' +
-        $('#menu .menutop-open').parent().map(function() { return this.id; }).get().join(',') +
-        '; path=/';
-
-      return false;
-    });
-
     // replace each comment edit form with comment text and a button that
     // opens the form
     $('#product-comment-list, #product-admin-comment-list')
